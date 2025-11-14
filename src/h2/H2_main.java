2182a70f3d8e8f19ea4c8e1343ce04265b20a8ed
linusgamahalfter-a11y <linus.gama.halfter@gmail.com>
2025-11-14 17:48:19 +0100

    Update H2_main: Boolean-Logik implementiert

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -2,9 +2,29 @@ package h2;
 
 public class H2_main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        boolean x, y, a, b, c;
+        int input = 0;
 
+        x = (input == 10 || input == 11);
+
+        y = (input == 11 || input == 1);
+
+        // a: E1 UND E2
+        a = (x && y);
+
+        // b: E1 ODER E3
+        b = (x || (x != y));
+
+        if (x && y) {
+            c = false;
+        } else if (x && !y) {
+            c = true;
+        } else if (!x && y) {
+            c = false;
+        } else {
+            c = true;
+        }
+    }
 }
