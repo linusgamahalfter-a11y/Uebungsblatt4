5c8b8036c89bfbb27564e8c723b40b30d727a7b4
linusgamahalfter-a11y <linus.gama.halfter@gmail.com>
2025-11-14 17:36:16 +0100

    Initial commit: Uebungsblatt4 hochladen

---
+++ src/h1/H1_main.java
@@ -0,0 +1,36 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		
+		int zone =4;
+		double price =0.00;
+		
+		switch (zone) {
+		case 1:
+		    price = 2.00;
+		    break;
+
+		case 2:
+		    price = 2.35;
+		    break;
+
+		case 3:
+		case 4:
+		    price = 2.85;
+		    break;
+
+		case 5:
+		    price = 3.55;
+		    break;
+
+		default:
+		    price = 4.00;
+		    break;
+		}
+		
+		System.out.print(price);;
+	}
+
+}
